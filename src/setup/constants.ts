aa50a9988645c07ee9390dc39bbe1ed671ec7ab6
Luma <you@example.com>
2024-05-10 17:07:30 -0700

    fix

--- src/setup/constants.ts
+++ src/setup/constants.ts
@@ -1,5 +1,5 @@
 export const APP_VERSION = import.meta.env.PACKAGE_VERSION;
-export const DISCORD_LINK = "https://discord.gg/r5cYshWM4G";
+export const DISCORD_LINK = "https://discord.gg/NaFVMW9vxS";
 export const GITHUB_LINK = "https://github.com/C-Luma/";
 export const EMAIL_LINK = "/Support";
 export const GA_ID = import.meta.env.VITE_GA_ID;
