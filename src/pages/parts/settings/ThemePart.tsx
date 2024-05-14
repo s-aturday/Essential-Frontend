4863798ccfe3868dcf63614917f4b4142b8dd079
Luma <you@example.com>
2024-05-14 08:12:40 -0700

    git commit -m "Update translations"

--- src/pages/parts/settings/ThemePart.tsx
+++ src/pages/parts/settings/ThemePart.tsx
@@ -30,6 +30,11 @@ const availableThemes = [
     selector: "theme-gray",
     key: "settings.appearance.themes.gray",
   },
+  {
+    id: "pink",
+    selector: "theme-pink",
+    key: "settings.appearance.themes.pink",
+  },
 ];
 
 function ThemePreview(props: {
