74687f5d1d056bb48a97e8bf45dc90f1fbfe35c1
Luma <you@example.com>
2024-05-14 08:06:38 -0700

    added theme

--- themes/all.ts
+++ themes/all.ts
@@ -2,9 +2,11 @@ import teal from "./list/teal";
 import blue from "./list/blue";
 import red from "./list/red";
 import gray from "./list/gray";
+import pink from "./list/pink";
 
 export const allThemes = [
   teal,
+  pink,
   blue,
   gray,
   red
